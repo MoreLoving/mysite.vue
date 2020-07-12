e696612ea15bf47783e3aefc05222e841b128057
joker <1164726219@qq.com>
2020-07-12 16:03:55 +0800

    修改url_column

--- src/views/category.vue
+++ src/views/category.vue
@@ -64,6 +64,9 @@ export default {
                     this.blog_list = res.data.results
                 }
                 this.next = res.data.next
+                if(res.data.next != null){
+                    this.get_blog(res.data.next, type, kw, true)
+                }
                 if(type == 'search'){
                     this.bus.$emit("ChangeBreadcrumb",[kw+' 相关的文章'])
                 }else{
@@ -79,24 +82,24 @@ export default {
         },
     },
     mounted(){
-        var self = this
-        window.onscroll = function(){
-            //变量scrollTop是滚动条滚动时，距离顶部的距离
-            var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
-            //变量windowHeight是可视区的高度
-            var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
-            //变量scrollHeight是滚动条的总高度
-            var scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
-            //滚动条到底部的条件
-            if(scrollTop+windowHeight==scrollHeight){
-                //写后台加载数据的函数
-                if(self.next != null){
-                    var type = self.$route.params.type
-                    var kw= self.$route.query.kw
-                    self.get_blog(self.next, type, kw, true)
-                }
-            }
-        }
+        // var self = this
+        // window.onscroll = function(){
+        //     //变量scrollTop是滚动条滚动时，距离顶部的距离
+        //     var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+        //     //变量windowHeight是可视区的高度
+        //     var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+        //     //变量scrollHeight是滚动条的总高度
+        //     var scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
+        //     //滚动条到底部的条件
+        //     if(scrollTop+windowHeight==scrollHeight){
+        //         //写后台加载数据的函数
+        //         if(self.next != null){
+        //             var type = self.$route.params.type
+        //             var kw= self.$route.query.kw
+        //             self.get_blog(self.next, type, kw, true)
+        //         }
+        //     }
+        // }
     },
     activated(){
         var type = this.$route.params.type
